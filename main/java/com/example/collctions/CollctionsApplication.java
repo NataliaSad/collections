bce3c89b998ab63155787d22a3606a802fddef0b
ЛЁША <kovnatali@mail.ru>
2023-04-10 22:50:41 +0700

    first commit

---
+++ main/java/com/example/collctions/CollctionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.collctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollctionsApplication.class, args);
+    }
+
+}
---
+++ test/java/com/example/collctions/CollctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollctionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
